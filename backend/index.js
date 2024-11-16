5206d1f67e38fac2e25de78e4b9868523a21c2ec
gsunil1996 <gajulasunilkumar123@gmail.com>
2024-11-16 21:42:00 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieSession from "cookie-session";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
