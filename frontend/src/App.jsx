dc230b489ea7bc863dc79b30291939268e7d66db
gsunil1996 <gajulasunilkumar123@gmail.com>
2024-11-17 10:56:25 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import PrivateRoutes from "./PrivateRoutes";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
-import { useSelector } from "react-redux";
 
 const App = () => {
-  const { isAuthenticated } = useSelector(state => state.auth);
 
   return (
     <Routes>
       {/* If the user is authenticated, redirect them from login to the home page */}
-      <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <Login />} />
+      <Route path="/login" element={<Login />} />
 
       {/* Private routes require validation before access */}
       <Route path="/" element={<PrivateRoutes />}>
